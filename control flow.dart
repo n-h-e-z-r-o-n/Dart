0fe19be833024db7d4cca858c22de46a5e1157b9
Hezron Na <hezron.w12@gmail.com>
2024-03-26 11:12:27 +0300

    Committed 2024 0Tu.111227

---
+++ control flow.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number:");
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
